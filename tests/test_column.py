51d03c2b3cb6fde6a464d72d600e3326c0db500d
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-16 22:58:20 +0000

    columns, models complete, tests passing

--- tests/test_column.py
+++ tests/test_column.py
@@ -1,8 +1,19 @@
+
 from flask_file_upload.file_upload import FileUpload
 from tests.fixtures.app import db
 
 
 class TestColumn:
+
+    test_results = {
+        "my_video_file_name": "video1",
+        "my_video_mime_type": "video/mpeg",
+        "my_video_file_type": "mp4",
+        "my_placeholder_file_name": "placeholder1",
+        "my_placeholder_mime_type": "image/jpeg",
+        "my_placeholder_file_type": "jpg",
+        "id": 1,
+    }
 
     def test_column(self):
 
@@ -15,7 +26,7 @@ class TestColumn:
             my_placeholder = file_upload.Column(db)
             my_video = file_upload.Column(db)
 
-        model_test = ModelTest()
+        model_test = ModelTest(**self.test_results)
 
         assert hasattr(model_test, "my_video_file_name")
         assert hasattr(model_test, "my_video_mime_type")
@@ -25,6 +36,14 @@ class TestColumn:
         assert hasattr(model_test, "my_placeholder_mime_type")
         assert hasattr(model_test, "my_placeholder_file_type")
         assert not hasattr(model_test, "my_placeholder")
+        assert hasattr(model_test, "id")
 
+        assert model_test.my_video_file_name == "video1"
+        assert model_test.my_video_mime_type == "video/mpeg"
+        assert model_test.my_video_file_type == "mp4"
+        assert model_test.my_placeholder_file_name == "placeholder1"
+        assert model_test.my_placeholder_mime_type == "image/jpeg"
+        assert model_test.my_placeholder_file_type == "jpg"
+        assert model_test.id == 1
 
 
