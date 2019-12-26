5bbc02cd527a740562f7c00a752c1819bbf875f9
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-26 23:42:44 +0000

    Update files #7       - model updated       - 13/13 tests passing

--- tests/test_file_upload.py
+++ tests/test_file_upload.py
@@ -48,22 +48,22 @@ class TestFileUploads:
 
     def setup_method(self):
         # Copy files from asset dir to test dir here:
+        app.config["UPLOAD_FOLDER"] = "tests/test_path"
         copyfile("tests/assets/my_video.mp4", "tests/test_path/blogs/1/my_video.mp4")
 
     def teardown_method(self):
         # Delete the files from the test dir here:
         try:
-            pass
             os.remove("tests/test_path/blogs/1/my_video_updated.mp4")
         except:
             pass
-
+    @pytest.mark.d
     def test_init_app(self, create_app, mock_blog_model, flask_app):
 
         file_upload = FileUpload()
         file_upload.init_app(flask_app)
         assert isinstance(file_upload.app, Flask)
-
+    @pytest.mark.d
     def test_set_model_attrs(self, mock_model):
         file_upload = FileUpload()
         file_upload.file_data = self.file_data
@@ -80,19 +80,12 @@ class TestFileUploads:
             file_upload.file_data[0]["bananas"] = "bananas"
             file_upload._set_model_attrs(mock_model)
 
-    def test_save_files(self, create_app):
-        data = {
-            "my_video": (self.my_video, "my_video.mp4"),
-            "my_placeholder": (self.my_placeholder, "my_placeholder.png")
-        }
-        rv = create_app.post("/blog", data=data, content_type="multipart/form-data")
-        assert "200" in rv.status
 
     def test_stream_file(self, create_app):
         rv = create_app.get("/blog")
         assert "200" in rv.status
 
-    @pytest.mark.d
+
     def test_update_files(self, create_app, mock_blog_model):
         file_upload.config.upload_folder = "tests/test_path"
 
@@ -125,35 +118,42 @@ class TestFileUploads:
         assert "my_video.mp4" not in os.listdir("tests/test_path/blogs/1")
 
 
-    # @pytest.mark.g
-    # def test_update_files_2(self, mock_blog_model):
-    #     db.init_app(app)
-    #     db.create_all()
-    #     file_upload = FileUpload()
-    #     file_upload.init_app(app)
-    #
-    #     new_file = FileStorage(
-    #             stream=open(self.my_video_update, "rb"),
-    #             filename="my_video_updated.mp4",
-    #             content_type="video/mpeg",
-    #         )
-    #
-    #     model = mock_blog_model(**self.attrs)
-    #
-    #     assert model.my_video__file_name == "my_video.mp4"
-    #     assert model.my_video__mime_type == "video/mpeg"
-    #     assert model.my_video__file_type == "mp4"
-    #
-    #     result = file_upload.update_files(
-    #         model,
-    #         files={"my_video": new_file},
-    #     )
-    #
-    #     assert result.my_video__file_name == "my_video_updated.mp4"
-    #     assert result.my_video__mime_type == "mp4"
-    #     assert result.my_video__file_type == "video/mpeg"
-    #
+    def test_update_files_2(self, mock_blog_model):
 
+        db.init_app(app)
+        db.create_all()
+        file_upload = FileUpload()
+        file_upload.init_app(app)
+
+        new_file = FileStorage(
+                stream=open(self.my_video_update, "rb"),
+                filename="my_video_updated.mp4",
+                content_type="video/mpeg",
+            )
+
+        model = mock_blog_model(**self.attrs)
+
+        assert model.my_video__file_name == "my_video.mp4"
+        assert model.my_video__mime_type == "video/mpeg"
+        assert model.my_video__file_type == "mp4"
+
+        result = file_upload.update_files(
+            model,
+            files={"my_video": new_file},
+        )
+
+        assert result.my_video__file_name == "my_video_updated.mp4"
+        assert result.my_video__mime_type == "mp4"
+        assert result.my_video__file_type == "video/mpeg"
+
+    def test_save_files(self, create_app):
+        """This test resets the upload folder so needs to be run last"""
+        data = {
+            "my_video": (self.my_video, "my_video.mp4"),
+            "my_placeholder": (self.my_placeholder, "my_placeholder.png")
+        }
+        rv = create_app.post("/blog", data=data, content_type="multipart/form-data")
+        assert "200" in rv.status
 
     def test_delete_files(self):
         pass
