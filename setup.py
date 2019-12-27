4eeee62151b0f95678b383679ded6cf94d6a80eb
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-27 21:05:23 +0000

    Public method `delete_files` #21       - all tests passing

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="flask-file-upload",
     version="0.0.1",
     description="Library that works with Flask & SqlAlchemy to store files in your database and server.",
     py_modules=["flask_file_upload"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joegasewicz/Flask-File-Upload",
+    author="Joe Gasewicz",
+    author_email="joegasewicz@gmail.com",
 )
