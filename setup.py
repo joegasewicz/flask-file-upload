b0ee1d91967486bff36bbeb68f544a0a5ede6556
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-29 18:19:09 +0000

    license

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="flask-file-upload",
-    version="0.0.1",
+    version="0.0.2",
     description="Library that works with Flask & SqlAlchemy to store files in your database and server.",
     py_modules=["flask_file_upload"],
     classifiers=[
