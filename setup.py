05cdc4b880e3f90b4d7c10e0f6a0cfc2a90eebd7
Joe Gasewicz <joegasewicz@gmail.com>
2019-12-31 13:27:25 +0000

    version bump to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="flask-file-upload",
-    version="0.0.6",
+    version="0.0.7",
     description="Library that works with Flask & SqlAlchemy to store files in your database and server.",
     packages=["flask_file_upload"],
     py_modules=["flask_file_upload"],
